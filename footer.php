cb2be42d0c8543fda5823ead2dd3aca863a5f02e
TienTruong2002 <tientvgcc210283@fpt.edu.vn>
2023-10-25 21:36:49 +0700

    update

--- footer.php
+++ footer.php
@@ -1,10 +1,12 @@
         <div class="b-example-divider"></div>
             <footer class="text-muted py-5">
                 <div class="container">
-                    <p class="float-end mb-1">
-                        <a href="#">Back to top</a>
-                    </p>
-                    <p>&copy; 2017-2022 company, Inc.&middot; <a href="#">Terms Privacy</a></p>
+                    <h2>Store</h2>
+                    <p>842 Su Van Hanh, Ward 12, District 10, HCMC</p>
+                    <p>54, Mau Than Street, Xuan Khanh Ward, Ninh Kieu District, Can Tho</p>
+                    <p>139E Nguyen Trai, Ben Thanh Ward, District 1, HCMC</p>
+                    <p>The New Playground, 04, Quận 1, HCM</p>
+                    <p>&copy; 2017-2022 company, Inc.Terms Privacy&middot;</p><p class="float-end mb-1"><a href="#">Back to top</a></p>
                 </div>
             </footer>
         </body>
